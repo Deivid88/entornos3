e9718828c66c87224b09d8172816ecd874b9621c
Deivid88 <210154@iessierradeguara.com>
2022-02-07 12:16:37 +0100

    Esto es un proyecto para sumar 3 + 3

---
+++ test/src/test1/Calc.java
@@ -0,0 +1,16 @@
+package test1;
+
+public class Calc {
+
+	public static void main(String[] args) {
+		int a = 3;
+		int b = 3;
+		int resultado;
+		
+		resultado = a + b;
+		
+		System.out.println(resultado);
+
+	}
+
+}
